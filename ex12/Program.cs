12d69bfadd2cc8864523b8d8f022fa1dc06e5488
murabye <vlada_kuz2011@mail.ru>
2017-06-04 15:53:27 +0500

    сортировка простым выбором

--- ex12/Program.cs
+++ ex12/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace ex12
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+
         }
+
+
+        static void SimpleChoiseSort(int[] arr)
+        {
+            // сортировка простым выбором
+
+            var length = arr.Length;                    // количество проходов по массиву
+            for (var i = 0; i < length - 1; i++)
+            {
+                var min = i;                            // номер минимального
+
+                for (var j = i + 1; j < length; j++)    // поиск минимума
+                    if (arr[j] < arr[min])
+                        min = j;
+                
+                arr[i] += arr[min];                     // перестановка arr[i] и arr[min]
+                arr[min] = arr[i] - arr[min];           // без использования дополнительной
+                arr[i] -= arr[min];                     // переменной
+            }
+        }
+
+
     }
 }
