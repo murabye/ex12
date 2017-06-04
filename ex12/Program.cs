02bb3973f1836c2ffffcca2cd4cb954140b10348
murabye <vlada_kuz2011@mail.ru>
2017-06-04 16:37:50 +0500

    MSD, разобраться получше але

--- ex12/Program.cs
+++ ex12/Program.cs
@@ -33,13 +33,38 @@ namespace ex12
                         min = j;
                 }
 
-                countChange++;
-                arr[i] += arr[min];                     // перестановка arr[i] и arr[min]
-                arr[min] = arr[i] - arr[min];           // без использования дополнительной
-                arr[i] -= arr[min];                     // переменной
+                countChange++;                          // что считать здесь перестановкой?
+                arr[i] += arr[min];                     
+                arr[min] = arr[i] - arr[min];
+                arr[i] -= arr[min];                     
             }
         }
 
+        static void MSD(int[] arr)
+        {
+            // разобраться получше!
 
+            int i, j;
+            var temp = new int[arr.Length];
+
+            for (var shift = 31; shift > -1; --shift)       // от последнего к первому разряду
+            {
+                j = 0;
+                for (i = 0; i < arr.Length; ++i)
+                {
+                    countEqual++;
+                    countChange++;                          // спросить считается ли по временному массиву движуха
+                    var move = (arr[i] << shift) >= 0;      // результат смещения бита (деления на 2) >= нуля?
+                    if (shift == 0 ? !move : move)          // перемещаем 0-ой в начало
+                        arr[i - j] = arr[i];
+                    else                                    // перемещаем 1-ый во временный массив
+                        temp[j++] = temp[i];
+                }
+
+                Array.Copy(temp, 0, arr, arr.Length - j, j);    // можно ли?
+            }
+
+
+        }
     }
 }
